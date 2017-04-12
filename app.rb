54e68bc7a4a4ab3c7c37d50174e8c8134e42f6e7
Kepuvv <renard.ru@gmail.com>
2017-04-12 14:27:14 +0300

    show latest posts

--- app.rb
+++ app.rb
@@ -25,7 +25,12 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+
+	#выбираем список постов из БД
+
+	@results = @db.execute 'SELECT * FROM Posts ORDER BY id DESC'
+
+	erb :index
 end
 
 get '/new' do
